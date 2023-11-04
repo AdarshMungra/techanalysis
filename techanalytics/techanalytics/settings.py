db8058b7f4b9e8baad9c307a0f906a5727037131
AdarshMungra <adarshmungra1@gmail.com>
2023-11-04 17:39:17 +0400

    initial setup 3 added views and models

--- techanalytics/techanalytics/settings.py
+++ techanalytics/techanalytics/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'techanalytics.views', #custom views location
+    'techanalytics.models', # cusotm models location 
 ]
 
 MIDDLEWARE = [
