e45aeb22cc4a7a7145f6329562ce83ff47f5c13c
AdarshMungra <adarshmungra1@gmail.com>
2023-11-05 18:10:57 +0400

    virtual env

--- techanalytics/techanalytics/views.py
+++ techanalytics/techanalytics/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, this is a minimal home page.")
+    return render(request, 'home.html')
